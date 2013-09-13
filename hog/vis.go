eac95ed6b13c382aafa305df7c1809de4565f49d
Jack Valmadre <jack.valmadre@gmail.com>
2013-09-13 14:10:46 +1000

    Changed Image and SignedImage to Vis()
    Combined weights from 27 channels.
    Added absolute weights.

--- hog/vis.go
+++ hog/vis.go
@@ -1,81 +1,108 @@
 package hog
 
 import (
+	"code.google.com/p/draw2d/draw2d"
 	"github.com/jackvalmadre/go-cv"
 	"github.com/jackvalmadre/lin-go/vec"
-	"code.google.com/p/draw2d/draw2d"
 
 	"image"
 	"image/color"
+	"image/draw"
 	"math"
 )
 
-func Image(hog cv.RealVectorImage, cellSize int) image.Image {
-	width, height := hog.Width*cellSize, hog.Height*cellSize
-	pic := image.NewRGBA(image.Rect(0, 0, width, height))
+type WeightSet int
 
-	// Fill with black.
-	for i := 0; i < width; i++ {
-		for j := 0; j < height; j++ {
-			pic.Set(i, j, color.Gray{0})
-		}
+const (
+	Signed WeightSet = iota
+	Pos
+	Neg
+	Abs
+)
+
+func Vis(feat cv.RealVectorImage, weights WeightSet, cell int) image.Image {
+	if feat.Channels == 31 {
+		return Vis(compress(feat, weights), weights, cell)
 	}
 
-	// Maximum value in vectorized image.
-	max, _ := vec.Max(hog.Vec())
+	width, height := feat.Width*cell, feat.Height*cell
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
 
-	gc := draw2d.NewGraphicContext(pic)
+	// Fill background.
+	bg := color.Gray{0}
+	if weights == Signed {
+		bg = color.Gray{128}
+	}
+	draw.Draw(img, img.Bounds(), image.NewUniform(bg), image.ZP, draw.Src)
+
+	// Rescale intensities to [0, 1].
+	var dst vec.Mutable = feat.Vec()
+	var src vec.Const = dst
+
+	if weights == Signed {
+		max, _ := vec.Max(vec.Abs(src))
+		rescale := func(x float64) float64 {
+			return (1+x/max) / 2
+		}
+		vec.Copy(dst, vec.Map(src, rescale))
+	} else {
+		switch weights {
+		case Neg:
+			src = vec.Scale(-1, src)
+		case Abs:
+			src = vec.Abs(src)
+		default:
+		}
+
+		max, _ := vec.Max(src)
+		rescale := func(x float64) float64 {
+			return math.Max(0, x/max)
+		}
+		vec.Copy(dst, vec.Map(src, rescale))
+	}
+
+	gc := draw2d.NewGraphicContext(img)
 	gc.SetLineWidth(1)
 
 	// Draw cells.
-	for x := 0; x < hog.Width; x++ {
-		for y := 0; y < hog.Height; y++ {
-			drawCell(hog, x, y, gc, cellSize, 0, max)
+	for x := 0; x < feat.Width; x++ {
+		for y := 0; y < feat.Height; y++ {
+			drawCell(feat, x, y, gc, cell)
 		}
 	}
-
-	return pic
+	return img
 }
 
-func SignedImage(hog cv.RealVectorImage, cellSize int) image.Image {
-	width, height := hog.Width*cellSize, hog.Height*cellSize
-	pic := image.NewRGBA(image.Rect(0, 0, width, height))
-
-	// Fill with gray.
-	for i := 0; i < width; i++ {
-		for j := 0; j < height; j++ {
-			pic.Set(i, j, color.Gray{128})
+// Flattens 31 (or 27) channels down to 9 for visualization.
+func compress(src cv.RealVectorImage, weights WeightSet) cv.RealVectorImage {
+	dst := cv.NewRealVectorImage(src.Width, src.Height, 9)
+	for i := 0; i < 27; i++ {
+		for x := 0; x < src.Width; x++ {
+			for y := 0; y < src.Height; y++ {
+				v := src.At(x, y, i)
+				switch weights {
+				default:
+				case Pos:
+					v = math.Max(0, v)
+				case Neg:
+					v = math.Min(0, v)
+				case Abs:
+					v = math.Abs(v)
+				}
+				dst.Set(x, y, i%9, dst.At(x, y, i%9)+v)
+			}
 		}
 	}
-
-	// Maximum absolute value in vectorized image.
-	max := vec.InfNorm(hog.Vec())
-
-	gc := draw2d.NewGraphicContext(pic)
-	gc.SetLineWidth(2)
-
-	// Draw cells.
-	for x := 0; x < hog.Width; x++ {
-		for y := 0; y < hog.Height; y++ {
-			drawCell(hog, x, y, gc, cellSize, -max, max)
-		}
-	}
-
-	return pic
+	return dst
 }
 
-func drawCell(hog cv.RealVectorImage, i, j int, gc *draw2d.ImageGraphicContext, cellSize int, min, max float64) {
-	u := (float64(i) + 0.5) * float64(cellSize)
-	v := (float64(j) + 0.5) * float64(cellSize)
-	r := float64(cellSize) / 2
-
-	offset := 0
-	if hog.Channels == 31 {
-		offset += 18
-	}
+func drawCell(feat cv.RealVectorImage, i, j int, gc *draw2d.ImageGraphicContext, cell int) {
+	u := (float64(i) + 0.5) * float64(cell)
+	v := (float64(j) + 0.5) * float64(cell)
+	r := float64(cell) / 2
 
 	for k := 0; k < Orientations; k++ {
-		x := (hog.At(i, j, offset+k) - min) / (max - min)
+		x := feat.At(i, j, k)
 		x = math.Max(x, 0)
 		x = math.Min(x, 1)
 		gc.SetStrokeColor(color.Gray{uint8(x*254 + 1)})
