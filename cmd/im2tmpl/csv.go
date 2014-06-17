f8538c65a3c0d1cbe6716e4b0232561208642de7
Jack Valmadre <jack.valmadre@gmail.com>
2014-06-18 01:44:03 +1000

    Convert weights to feature template

---
+++ cmd/im2tmpl/csv.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/csv"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/jackvalmadre/go-cv/rimg64"
+)
+
+func loadImageCSV(fname string) (*rimg64.Multi, error) {
+	file, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return readImageCSV(file)
+}
+
+func readImageCSV(r io.Reader) (*rimg64.Multi, error) {
+	cr := csv.NewReader(r)
+	rows, err := cr.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	return parseImageCSV(rows)
+}
+
+func parseImageCSV(rows [][]string) (*rimg64.Multi, error) {
+	// Convert to numbers.
+	var (
+		u, v, w []int
+		f       []float64
+	)
+	for _, r := range rows {
+		if len(r) == 0 {
+			continue
+		}
+		if len(r) != 4 {
+			panic("wrong number of elements in row")
+		}
+		ui, err := strconv.ParseInt(r[0], 10, 32)
+		if err != nil {
+			return nil, err
+		}
+		vi, err := strconv.ParseInt(r[1], 10, 32)
+		if err != nil {
+			return nil, err
+		}
+		wi, err := strconv.ParseInt(r[2], 10, 32)
+		if err != nil {
+			return nil, err
+		}
+		fi, err := strconv.ParseFloat(r[3], 64)
+		if err != nil {
+			return nil, err
+		}
+		u = append(u, int(ui))
+		v = append(v, int(vi))
+		w = append(w, int(wi))
+		f = append(f, fi)
+	}
+
+	// Take max over u, v, w.
+	var width, height, channels int
+	for i := range u {
+		width = max(u[i]+1, width)
+		height = max(v[i]+1, height)
+		channels = max(w[i]+1, channels)
+	}
+
+	// Set pixels in image.
+	im := rimg64.NewMulti(width, height, channels)
+	for i := range u {
+		im.Set(u[i], v[i], w[i], f[i])
+	}
+	return im, nil
+}
+
+func saveImageCSV(fname string, im *rimg64.Multi) error {
+	file, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return writeImageCSV(file, im)
+}
+
+func writeImageCSV(w io.Writer, im *rimg64.Multi) error {
+	cw := csv.NewWriter(w)
+	rows := formatImageCSV(im)
+	return cw.WriteAll(rows)
+}
+
+func formatImageCSV(im *rimg64.Multi) [][]string {
+	var rows [][]string
+	for u := 0; u < im.Width; u++ {
+		for v := 0; v < im.Height; v++ {
+			for w := 0; w < im.Channels; w++ {
+				r := make([]string, 4)
+				r[0] = strconv.FormatInt(int64(u), 10)
+				r[1] = strconv.FormatInt(int64(v), 10)
+				r[2] = strconv.FormatInt(int64(w), 10)
+				r[3] = strconv.FormatFloat(im.At(u, v, w), 'g', -1, 64)
+				rows = append(rows, r)
+			}
+		}
+	}
+	return rows
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
---
+++ cmd/im2tmpl/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/gob"
+	"flag"
+	"fmt"
+	"image"
+	"os"
+	"path"
+
+	"github.com/jackvalmadre/go-cv/detect"
+	"github.com/jackvalmadre/go-cv/rimg64"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s weights.(gob|csv)\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+func main() {
+	var (
+		width  = flag.Int("width", 0, "Width in pixels")
+		height = flag.Int("height", 0, "Height in pixels")
+		top    = flag.Int("top", 0, "Inset from pixel size to give interior")
+		left   = flag.Int("left", 0, "Inset from pixel size to give interior")
+		bottom = flag.Int("bottom", 0, "Inset from pixel size to give interior")
+		right  = flag.Int("right", 0, "Inset from pixel size to give interior")
+	)
+
+	flag.Parse()
+	flag.Usage = usage
+	if flag.NArg() != 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	imname := flag.Arg(0)
+	tmplname := flag.Arg(1)
+
+	imfile, err := os.Open(imname)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer imfile.Close()
+
+	var weights *rimg64.Multi
+	if path.Ext(imname) == ".csv" {
+		var err error
+		weights, err = readImageCSV(imfile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	} else {
+		if err := gob.NewDecoder(imfile).Decode(&weights); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+
+	tmpl := &detect.FeatTmpl{
+		weights,
+		image.Pt(*width, *height),
+		image.Rect(*left, *top, *width-*right, *height-*bottom),
+	}
+
+	tmplfile, err := os.Create(tmplname)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer tmplfile.Close()
+
+	if err := gob.NewEncoder(tmplfile).Encode(tmpl); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
