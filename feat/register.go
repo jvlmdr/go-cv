fe42655021be30d0743d26b28d568612256503f7
Jack Valmadre <jack.valmadre@gmail.com>
2014-08-22 16:05:52 -0400

    Add feat.Unmarshaler which uses global Transforms

--- feat/register.go
+++ feat/register.go
@@ -1,7 +1,38 @@
 package feat
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 var Transforms = make(map[string]func() Transform)
 
 func Register(name string, create func() Transform) {
 	Transforms[name] = create
+}
+
+type Unmarshaler struct {
+	Name string
+	Spec Transform
+}
+
+func (m *Unmarshaler) UnmarshalJSON(data []byte) error {
+	var x struct {
+		Name string
+		Spec json.RawMessage
+	}
+	if err := json.Unmarshal(data, &x); err != nil {
+		return err
+	}
+	create, ok := Transforms[x.Name]
+	if !ok {
+		return fmt.Errorf(`unknown feature: "%s"`, x.Name)
+	}
+	t := create()
+	if err := json.Unmarshal(x.Spec, t); err != nil {
+		return err
+	}
+	m.Name = x.Name
+	m.Spec = t
+	return nil
 }
