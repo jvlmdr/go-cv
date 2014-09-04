d2b1f90b7da29f3c94d1cbbdbfb0d3b2aca6ae23
Jack Valmadre <jack.valmadre@gmail.com>
2014-09-04 14:54:53 -0400

    Add MultiScale() function

--- detect/pyr.go
+++ detect/pyr.go
@@ -1,10 +1,39 @@
 package detect
 
 import (
+	"image"
+
+	"github.com/jvlmdr/go-cv/feat"
 	"github.com/jvlmdr/go-cv/featpyr"
 	"github.com/jvlmdr/go-cv/imgpyr"
 	"github.com/jvlmdr/go-cv/rimg64"
+	"github.com/nfnt/resize"
 )
+
+// MultiScaleOpts specifies the parameters to MultiScale().
+type MultiScaleOpts struct {
+	PyrStep float64
+	Interp  resize.InterpolationFunction
+	feat.Transform
+	feat.Pad
+	DetFilter
+	OverlapFunc
+}
+
+// MultiScale searches an image at multiple scales and performs non-max suppression.
+//
+// At each level, the image is rescaled using Interp,
+// then padded using Pad before calling Transform.Apply().
+// The levels are geometrically spaced at intervals of PyrStep.
+// Detections are filtered using DetFtiler and then non-max suppression
+// is performed using the OverlapFunc test.
+func MultiScale(tmpl *FeatTmpl, im image.Image, opts MultiScaleOpts) []Det {
+	scales := imgpyr.Scales(im.Bounds().Size(), tmpl.Size, opts.PyrStep)
+	pyr := featpyr.NewPad(imgpyr.NewInterp(im, scales, opts.Interp), opts.Transform, opts.Pad)
+	// Do not impose a maximum number of detections at each layer.
+	supprOpts := SupprFilter{MaxNum: 0, Overlap: opts.OverlapFunc}
+	return Pyramid(pyr, tmpl, opts.DetFilter, supprOpts)
+}
 
 // Performs detection and non-max suppression.
 // Returns a list of scored detection windows.
